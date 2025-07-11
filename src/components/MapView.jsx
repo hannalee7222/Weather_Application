831f6f263c4c4c96742538fc2c82f282d29d010f
hannalee7222 <hannah.lee7222@gmail.com>
2025-07-11 15:15:20 +0900

    🐛 Fix(map): 마커 이미지 경로 지정
    마커 아이콘 안 보이던 문제 해결(leaflet 이미지 경로 설정)

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,5 +1,19 @@
 import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import styled from 'styled-components';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapWrapper = styled.div`
   height: 300px;
